3a28e1757f04e5c616fe9605db65ffe17257bc65
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-10-27 15:25:33 +0800

    perf(index.ts): loadhomeshow()

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,19 +1,17 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const homeshowcontainer = document.getElementById("home-full-show-container");
-    const pagepicker = homeshowcontainer?.getElementsByClassName("page-picker")[0];
-    const pageshower = homeshowcontainer?.getElementsByClassName("page-shower")[0];
-    const maxgpid = pageshower?.childElementCount as number - 1 || 0;
+const loadhomeshow = (homeshowcontainer: HTMLDivElement) => {
+    const pagepicker = homeshowcontainer.querySelector(".page-picker");
+    const pageshower = homeshowcontainer.querySelector(".page-shower");
     if (pagepicker && pageshower) {
+        const maxgpid = pageshower.childElementCount - 1;
         for (const page of pageshower.children) {
-            const pageNumber = parseInt(page.getAttribute("gpid")?.toString() || "0");
             const picker = document.createElement("div");
             picker.classList.add(page.classList.contains("page-shower-this") ? "page-picker-this" : "page-picker-hide");
-            picker.setAttribute("gpid", pageNumber.toString());
+            picker.setAttribute("gpid", page.getAttribute("gpid") || "");
             pagepicker.appendChild(picker);
         }
-        pagepicker.addEventListener("click", (event: Event) => {
-            if (event.target instanceof HTMLDivElement && event.target.parentElement === pagepicker) {
-                const targetgpid = parseInt(event.target.getAttribute("gpid") || "0");
+        pagepicker.addEventListener("click", (e) => {
+            if (e.target instanceof HTMLDivElement && e.target.parentElement === pagepicker) {
+                const targetgpid = parseInt(e.target.getAttribute("gpid") || "0");
                 // shower
                 for (const page of pageshower.children) {
                     const pagegpid = parseInt(page.getAttribute("gpid") || "0");
@@ -29,14 +27,20 @@ window.addEventListener("DOMContentLoaded", () => {
                 }
                 // picker
                 for (const picker of pagepicker.children) {
-                    picker.className = "";
                     if (picker.getAttribute("gpid") === targetgpid.toString()) {
-                        picker.classList.add("page-picker-this");
+                        picker.className = "page-picker-this";
                     } else {
-                        picker.classList.add("page-picker-hide");
+                        picker.className = "page-picker-hide";
                     }
                 }
             }
         });
+    }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    const homeshowcontainer = document.getElementById("home-full-show-container");
+    if (homeshowcontainer && homeshowcontainer instanceof HTMLDivElement) {
+        loadhomeshow(homeshowcontainer);
     }
 });
